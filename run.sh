ac623dbc9ee5313e05b6678e33a81fdfdc0a3473
Jesper Mikkelsen <jesper_mikkelsen@trendmicro.com>
2022-12-15 11:50:05 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ echo "Ready:Core@Container:>ELastic server IP ${2} : added."
 export TMV1_TOKEN="${1}"
 export TMV1_ELASTIC_HOST="${2}"
 sleep 10
-echo "Ready:Core@Container:> RUNNING VisionOne import every 15 minutes.........."
+echo "Ready:Core@Container:> RUNNING VisionOne import every 60 minutes.........."
 
-while true; do python3 main.py ;date ; sleep 900; done
+while true; do python3 main.py ;date ; sleep 3600; done
